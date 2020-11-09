a30979d53fc82c5cc3060b6ca58ae4504a09fbc9
Fahad Maqbool <fahadmaqbool28888@gmail.com>
2020-11-09 15:13:02 +0500

    Initial commit

---
+++ app/src/main/java/com/mandi/perfectmandii/Model/DataFish.java
@@ -0,0 +1,11 @@
+package com.mandi.perfectmandii.Model;
+
+public class DataFish
+{
+    public String fishImage;
+    public String fishName;
+    public String catName;
+    public String sizeName;
+    public int price;
+    public String usid;
+}
---
+++ app/src/main/java/com/mandi/perfectmandii/Model/Subject.java
@@ -0,0 +1,14 @@
+package com.mandi.perfectmandii.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ashish on 06-05-2018.
+ */
+
+public class Subject {
+    public int id;
+    public String subjectName;
+
+    //public ArrayList<com.mandi.sellermandi.Model.Chapter> chapters;
+}
